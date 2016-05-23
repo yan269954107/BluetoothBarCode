20c79086c3152247dc90532c5ad4efc9d19e6952
angeloyxw <angeloyxw@gmail.com>
2016-05-23 22:41:03 +0800

    add Test variants

---
+++ app/src/own/java/com/yanxinwei/bluetoothspppro/util/Test.java
@@ -0,0 +1,12 @@
+package com.yanxinwei.bluetoothspppro.util;
+
+/**
+ * Created by yanxinwei on 16/3/28.
+ */
+public class Test {
+
+    public static final int P = 1488445912;
+
+    public static void settime(String a,String b){}
+
+}
